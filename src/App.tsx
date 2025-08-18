e48130e2a8e4773359f97bf792a03a41815cf6e3
sgc <hsgchng@naver.com>
2025-08-18 15:53:24 +0900

    로컬 변경사항 커밋 - index.html 등

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
+import Home from './pages/Home';
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
+  return <Home />;
 }
